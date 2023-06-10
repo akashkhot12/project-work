1147a26332dea8f10c09dddef79ce3f90bcc2493
Akash khot <akashkhot03@gmail.com>
2023-06-10 11:39:04 +0530

    changes

--- angular-material-form/src/app/app.component.ts
+++ angular-material-form/src/app/app.component.ts
@@ -1,12 +1,26 @@
 import { Component } from '@angular/core';
+import {FormControl, FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {NgFor} from '@angular/common';
+import {MatAutocompleteModule} from '@angular/material/autocomplete';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  standalone: true,
+  imports: [
+    FormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatAutocompleteModule,
+    ReactiveFormsModule,
+    NgFor,
+  ],
 })
 export class AppComponent {
-[x: string]: any;
   title = 'angular-material-form';
+  myControl = new FormControl('');
   options: string[] = ['One', 'Two', 'Three'];
 }
