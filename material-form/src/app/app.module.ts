887b5eb2d578f3908849e0f0c8efc87e885f6ece
Akash khot <akashkhot03@gmail.com>
2023-06-10 13:35:00 +0530

    new prroject on material form

--- material-form/src/app/app.module.ts
+++ material-form/src/app/app.module.ts
@@ -1,22 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule } from '@angular/common/http';
+
 import { AppRoutingModule } from './app-routing.module';
-import {MatInputModule} from '@angular/material/input';
-import {MatFormFieldModule} from '@angular/material/form-field';
 import { AppComponent } from './app.component';
 
 @NgModule({
- declarations:[AppComponent],
+  declarations: [
+    AppComponent
+  ],
   imports: [
     BrowserModule,
-    MatInputModule,
-    MatFormFieldModule,
-    HttpClientModule,
-    AppRoutingModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
-
