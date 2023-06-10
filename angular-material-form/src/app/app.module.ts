7f54629c7e1aafbdd6cbce08dbe74cab219fa99e
Akash khot <akashkhot03@gmail.com>
2023-06-10 13:24:11 +0530

    import module

--- angular-material-form/src/app/app.module.ts
+++ angular-material-form/src/app/app.module.ts
@@ -1,15 +1,19 @@
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { AppRoutingModule } from './app-routing.module';
 import {MatInputModule} from '@angular/material/input';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+ declarations:[AppComponent],
   imports: [
+    BrowserModule,
     MatInputModule,
     MatFormFieldModule,
+    HttpClientModule,
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
