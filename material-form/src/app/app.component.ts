248d62c9af3eb03f36acf6ad1a037ff248ab7217
Akash khot <akashkhot03@gmail.com>
2023-06-12 11:08:19 +0530

    additional changes

--- material-form/src/app/app.component.ts
+++ material-form/src/app/app.component.ts
@@ -1,37 +1,10 @@
-import { Component, VERSION } from '@angular/core';
-import { FormControl, Validators } from '@angular/forms';
+import { Component } from '@angular/core';
 
 @Component({
-  selector: "my-app",
-  templateUrl: "./app.component.html",
-  styleUrls: ["./app.component.css"]
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  name = "Angular " + VERSION.major;
-  display: FormControl = new FormControl("", Validators.required);
-  file_store!: FileList;
-  file_list: Array<string> = [];
-
-  handleFileInputChange(l: FileList): void {
-    this.file_store = l;
-    if (l.length) {
-      const f = l[0];
-      const count = l.length > 1 ? `(+${l.length - 1} files)` : "";
-      this.display.patchValue(`${f.name}${count}`);
-    } else {
-      this.display.patchValue("");
-    }
-  }
-
-  handleSubmit(): void {
-    var fd = new FormData();
-    this.file_list = [];
-    for (let i = 0; i < this.file_store.length; i++) {
-      fd.append("files", this.file_store[i], this.file_store[i].name);
-      this.file_list.push(this.file_store[i].name);
-    }
-
-    // do submit ajax
-  }
+  title = 'material-form';
 }
-
