e4a862299c396e00d6fe42def8ea11d77cc19eb7
Akash khot <akashkhot03@gmail.com>
2023-06-20 14:13:58 +0530

    adding new project

--- Weather-App/src/app/app.component.spec.ts
+++ Weather-App/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'wheather'`, () => {
+  it(`should have as title 'Weather-App'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('wheather');
+    expect(app.title).toEqual('Weather-App');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('wheather app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('Weather-App app is running!');
   });
 });
