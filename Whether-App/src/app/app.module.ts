f719a5f5e79d805132d94590e40587dd937bd2d3
akashkhot12 <akashkhot03@gmail.com>
2023-06-29 21:46:34 +0530

    create a weather app

--- Whether-App/src/app/app.module.ts
+++ Whether-App/src/app/app.module.ts
@@ -3,12 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { WeatherWidgetMainComponent } from './Components/weather-widget-main/weather-widget-main.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    WeatherWidgetMainComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
