d8c2a253969685974f3310268113832a568af058
akashkhot12 <akashkhot03@gmail.com>
2023-07-04 15:26:24 +0530

    create logic about timezone

--- Whether-App/src/app/weather/weather.component.ts
+++ Whether-App/src/app/weather/weather.component.ts
@@ -9,19 +9,35 @@ import { WeatherService } from '../weather.service';
 export class WeatherComponent {
  city!:string;
  weatherData:any;
+ src:string="";
+  time : any ; 
+ yourDate:any
 
- constructor(private weatherService:WeatherService){}
+ constructor(private weatherService:WeatherService){
+  let now = new Date();
+  this.time = now.toLocaleString();
+ }
+
 
  ngOnInit(){
 
  }
 
+ getImage(){
+  if(this.weatherData.weather[0].main === 'Clouds'){
+    this.src = "../../assets/image/clipart3532095.png" ; 
+  }else{
+    this.src = "../../assets/image/storm.png" ;
+  }
+ }
  getWeather(){
   this.weatherService.getWeather(this.city)
   .subscribe(data=>{
     this.weatherData=data;
+    this.getImage()
     console.log(data);
     
   })
  }
+ 
 }
