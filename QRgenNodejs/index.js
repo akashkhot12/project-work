098d4abffa25734d560277fc0c587b18bd7e782c
akashkhot12 <akashkhot03@gmail.com>
2023-09-27 11:11:15 +0530

    qr genratorr

--- QRgenNodejs/index.js
+++ QRgenNodejs/index.js
@@ -1,15 +1,19 @@
 import inquirer from "inquirer";
+import qr from "qr-image"
+
 inquirer.prompt([{
     message:"type in your URL",
     name:"URL"
 },
 ]).then((answer)=>{
     const url = answer.URL;
+    var qr_svg = qr.image(url);
+    qr_svg.pipe(require("fs").createWriteStream("qr-immg.png"));
 }).catch((error)=>{
     if (error.isTtyError) {
         
     }
     else{
-        
+
     }
 })
